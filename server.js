8fcfcbbb9edc03e04ab2834fea2b122d5330142e
quickmovies <tryy.spam@gmail.com>
2026-05-09 01:30:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req,res)=>{
@@ -15,18 +13,16 @@ app.get("/", (req,res)=>{
 
 app.post("/chat", async (req,res)=>{
 
-  const userMessage = req.body.message;
-
   try{
 
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_KEY}`,
       {
         contents:[
           {
             parts:[
               {
-                text:userMessage
+                text:req.body.message
               }
             ]
           }
@@ -35,8 +31,8 @@ app.post("/chat", async (req,res)=>{
     );
 
     const reply =
-      response.data.candidates[0]
-      .content.parts[0].text;
+    response.data.candidates[0]
+    .content.parts[0].text;
 
     res.json({
       reply
@@ -44,9 +40,7 @@ app.post("/chat", async (req,res)=>{
 
   }catch(err){
 
-    console.log(
-      err.response?.data || err.message
-    );
+    console.log(err.response?.data);
 
     res.json({
       reply:"Gemini API failed"
@@ -56,8 +50,7 @@ app.post("/chat", async (req,res)=>{
 
 });
 
-const PORT =
-process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
   console.log("Server running");
