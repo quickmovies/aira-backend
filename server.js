772ff0760cf4c0733d897aa373f22df86675bfa4
quickmovies <tryy.spam@gmail.com>
2026-05-09 03:19:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,29 +8,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.send("Aira Backend Running");
 });
 
-app.post("/chat", async (req,res)=>{
+app.post("/chat", async (req, res) => {
 
-  try{
+  try {
 
     const userMessage = req.body.message;
 
     const response = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_KEY}`,
       {
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json"
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
         },
-        body:JSON.stringify({
-          contents:[
+        body: JSON.stringify({
+          contents: [
             {
-              parts:[
+              parts: [
                 {
-                  text:`You are Aira, a caring AI assistant.\nUser: ${userMessage}`
+                  text:
+                  `You are Aira, a caring AI assistant.\nUser: ${userMessage}`
                 }
               ]
             }
@@ -41,23 +42,21 @@ app.post("/chat", async (req,res)=>{
 
     const data = await response.json();
 
-    console.log(data);
-
     const reply =
-    data.candidates?.[0]
-    ?.content?.parts?.[0]
-    ?.text;
+      data.candidates?.[0]
+      ?.content?.parts?.[0]
+      ?.text || "No response";
 
     res.json({
-      reply: reply || "No response from Gemini"
+      reply
     });
 
-  }catch(err){
+  } catch (error) {
 
-    console.log(err);
+    console.log(error);
 
     res.json({
-      reply:"Backend crashed"
+      reply: "AI server error"
     });
 
   }
@@ -66,6 +65,6 @@ app.post("/chat", async (req,res)=>{
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-  console.log("Server running");
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
