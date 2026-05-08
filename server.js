68631100d2536decab68db76bdf08120b7de8e2f
quickmovies <tryy.spam@gmail.com>
2026-05-09 03:37:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.post("/chat", async (req, res) => {
     const apiKey = process.env.GEMINI_KEY.trim();
 
     // Call the Gemini API
-    const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+        const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
       {
         method: "POST",
         headers: {
